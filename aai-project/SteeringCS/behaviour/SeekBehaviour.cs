ebe886ef84e0b7cdc3964f24555295b26d918ca6
Joshua <jpjvangelder@gmail.com>
2017-03-03 14:05:34 +0100

    Implement SeekBehaviour.cs
    removed unused imports in MovingEntity.cs

--- aai-project/SteeringCS/behaviour/SeekBehaviour.cs
+++ aai-project/SteeringCS/behaviour/SeekBehaviour.cs
@@ -4,12 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SteeringCS.util;
 
 namespace SteeringCS.behaviour
 {
-    public class SeekBehaviour 
+    public class SeekBehaviour
     {
-        // to do
-        
+        private readonly MovingEntity _movingEntity;
+        private readonly Vehicle _vehicle;        
+
+        public Vector2D Seek(Vector2D target)
+        {
+            Vector2D desiredVelocity = (target.Sub(_movingEntity.Pos)).Multiply(_vehicle.MaxSpeed);
+            return desiredVelocity.Sub(_vehicle.Velocity);
+        }
     }
 }
